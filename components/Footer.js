467a3a036e38b3765f555ad4de6a848ca70668a6
bhanu-100 <bhanusinghx100@gmail.com>
2024-01-02 13:17:34 +0530

    init 1234

--- components/Footer.js
+++ components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
             <p className="mt-2 text-sm text-gray-500 mx-1">wear the &lt;code/&gt; - Premium tshirt,hoodies,mugs and caps.</p>
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-center">
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-3">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">SHOP</h2>
               <nav className="list-none mb-10">
                 <li>
@@ -29,7 +29,7 @@ const Footer = () => {
                 </li>
               </nav>
             </div>
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-3">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">ABOUT</h2>
               <nav className="list-none mb-10">
                 <li>
@@ -46,7 +46,7 @@ const Footer = () => {
                 </li>
               </nav>
             </div>
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-3">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">POLICY</h2>
               <nav className="list-none mb-10">
                 <li>
@@ -63,7 +63,7 @@ const Footer = () => {
                 </li>
               </nav>
             </div>
-            <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <div className="lg:w-1/4 md:w-1/2 w-full px-3">
               <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">CONTACT</h2>
               <nav className="list-none mb-10">
                 <li>
@@ -87,36 +87,36 @@ const Footer = () => {
             <p className="text-gray-500 text-sm text-center sm:text-left">© 2023 Codewear.com — All Rights Are Reserved
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-              <Link href={"https://www.facebook.com/profile.php?id=100009914970814&mibextid=rS40aB7S9Ucbxw6v"}>
               <a className="text-gray-500">
+              <Link href={"https://www.facebook.com/profile.php?id=100009914970814&mibextid=rS40aB7S9Ucbxw6v"}>
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                 </svg>
-              </a>
               </Link>
-              <Link href={"https://twitter.com/@BhanuPr62387506"}>
+              </a>
               <a className="ml-3 text-gray-500">
+              <Link href={"https://twitter.com/@BhanuPr62387506"}>
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
-              </a>
               </Link>
-              <Link href={"https://www.instagram.com/bhanu_thakur_bt?igsh=OGQ5ZDc2ODk2ZA=="}>
+              </a>
               <a className="ml-3 text-gray-500">
+              <Link href={"https://www.instagram.com/bhanu_thakur_bt?igsh=OGQ5ZDc2ODk2ZA=="}>
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                 </svg>
-              </a>
               </Link>
-              <Link href={"https://www.linkedin.com/in/bhanu-pratap-singh-7b7841224?jobid=1234&lipi=urn%3Ali%3Apage%3Ad_jobs_easyapply_pdfgenresume%3BlPWOOMIMRtGG9JQAXGI6QA%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_jobs_easyapply_pdfgenresume-v02_profile"}>
+              </a>
               <a className="ml-3 text-gray-500">
+              <Link href={"https://www.linkedin.com/in/bhanu-pratap-singh-7b7841224?jobid=1234&lipi=urn%3Ali%3Apage%3Ad_jobs_easyapply_pdfgenresume%3BlPWOOMIMRtGG9JQAXGI6QA%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_jobs_easyapply_pdfgenresume-v02_profile"}>
                 <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="0" className="w-5 h-5" viewBox="0 0 24 24">
                   <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                   <circle cx="4" cy="4" r="2" stroke="none"></circle>
                 </svg>
-              </a>
               </Link>
+              </a>
             </span>
           </div>
         </div>
