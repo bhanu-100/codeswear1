f41d9c9b39a0394a144ad90347bdd79e34850128
bhanu-100 <bhanusinghx100@gmail.com>
2024-07-13 14:27:53 +0530

    lastcomit1

--- pages/admin/addadmin.js
+++ pages/admin/addadmin.js
@@ -101,7 +101,7 @@ const addadmin = ({userDetail}) => {
     </div>
 
               <div>
-                <button type="submit" className="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Add Product</button>
+                <button type="submit" className="flex w-full justify-center rounded-md bg-pink-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600">Add Admin</button>
               </div>
             </form>
           </div>
